78a95c467035fa7b8e4739e581fa0f25d702ad7f
mohller <leonel.rmh@gmail.com>
2026-04-10 17:21:10 +0200

    preparing package setup files

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+CRISP: Cosmic Ray Stochastic Interactions for Propagation
+
+A convenience package to compute quantities related to the propagation 
+of ultrahigh-energy cosmic rays based on closed form probability 
+distribution functions.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Leonel Morejon"
+__email__ = "leonel.morejon@uni-wuppertal.de"
--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='crisp-py',
-    version='0.1.0',
-    packages=['crisp-py'],
-    install_requires=[
-        'astropy',
-        'numpy',
-        'pandas',
-        'scipy',
-        'pint',
-        'sympy'        
-    ],
-    description='Cosmic Ray Stochastic Interactions for Propagation',
-    long_description='A package to compute stochastic cosmic ray photonuclear interactions with probability distributions.',
-    long_description_content_type='text/markdown',
-    author='Leonel Morejon',
-    author_email='leonel.morejon@uni-wuppertal.de',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-    ]
-)
+setup()
